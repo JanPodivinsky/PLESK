516f7cce0ec6cced554e40212ea98b3f54a81c11
JanPodivinsky <jan.podivinsky@seznam.cz>
2016-12-09 11:19:09 +0100

    Update Kalkulacka.java

--- TypovySystem/Kalkulacka/src/kalkulacka/Kalkulacka.java
+++ TypovySystem/Kalkulacka/src/kalkulacka/Kalkulacka.java
@@ -1,20 +1,3 @@
-	/*
-	 *	       __          __                __            
-	 *	  ____/ /__ _   __/ /_  ____  ____  / /__ _________
-	 *	 / __  / _ \ | / / __ \/ __ \/ __ \/ //_// ___/_  /
-	 *	/ /_/ /  __/ |/ / /_/ / /_/ / /_/ / ,< _/ /__  / /_
-	 *	\__,_/\___/|___/_.___/\____/\____/_/|_(_)___/ /___/
-	 *                                                   
-	 *                                                           
-	 *      TUTORIÁLY  <>  DISKUZE  <>  KOMUNITA  <>  SOFTWARE
-	 * 
-	 *	Tento zdrojový kód je součástí tutoriálů na programátorské 
-	 *	sociální síti DEVBOOK.CZ	
-	 *	
-	 *	Kód můžete upravovat pod licencí MIT, 
-	 *	tedy jak chcete, jen zmiňte odkaz na www.devbook.cz :-) 
-	 */
-
 package kalkulacka;
 
 import java.util.Scanner;
